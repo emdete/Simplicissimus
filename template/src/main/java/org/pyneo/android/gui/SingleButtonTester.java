e4b01de257908c6ea6765b6fa94e34406762d24a
M. Dietrich <mdt@emdete.de>
2014-03-20 23:40:51 +0100

    add template

---
+++ template/src/main/java/org/pyneo/android/gui/SingleButtonTester.java
@@ -0,0 +1,73 @@
+package org.pyneo.android.gui;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SingleButtonTester extends Activity {
+	static final String TAG = SingleButtonTester.class.getName();
+	static boolean DEBUG = true;
+	// static { DEBUG = Log.isLoggable("org.pyneo.android", Log.DEBUG); }
+
+	Context context;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (DEBUG) Log.d(TAG, "onCreate");
+		setContentView(R.layout.main);
+		context = getBaseContext();
+		Button button = (Button)findViewById(R.id.button);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (DEBUG) Log.d(TAG, "onClick");
+				doTest(context);
+			}
+		});
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		if (DEBUG) Log.d(TAG, "onStart");
+	}
+
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+		if (DEBUG) Log.d(TAG, "onRestart");
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (DEBUG) Log.d(TAG, "onResume");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (DEBUG) Log.d(TAG, "onPause");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		if (DEBUG) Log.d(TAG, "onStop");
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (DEBUG) Log.d(TAG, "onDestroy");
+	}
+
+	public void doTest(Context context) {
+		Button button = (Button)findViewById(R.id.button);
+		button.setText("Started");
+	}
+}
