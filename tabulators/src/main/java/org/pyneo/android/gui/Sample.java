6875bb3de496c19d1f9998275230a7578fe6eb4b
M. Dietrich <mdt@emdete.de>
2014-05-09 17:23:40 +0200

    back

--- tabulators/src/main/java/org/pyneo/android/gui/Sample.java
+++ tabulators/src/main/java/org/pyneo/android/gui/Sample.java
@@ -20,7 +20,19 @@ import android.widget.TextView;
 public class Sample extends FragmentActivity {
 	static final String TAG = Sample.class.getName();
 	static boolean DEBUG = true;
-	ObjectFragment[] fragments = new ObjectFragment[3];
+	static final String POSITION = "position";
+
+	public static class ObjectFragment extends Fragment {
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			Bundle args = getArguments();
+			int position = args.getInt(POSITION);
+			Log.d(TAG, "onCreateView: position=" + position);
+			View rootView = inflater.inflate(R.layout.fragment, container, false);
+			((TextView)rootView.findViewById(android.R.id.text1)).setText(Integer.toString(position * 3 + 2));
+			return rootView;
+		}
+	}
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -30,52 +42,27 @@ public class Sample extends FragmentActivity {
 			@Override
 			public Fragment getItem(int position) {
 				Log.d(TAG, "getItem: position=" + position);
-				return fragments[position];
+				Fragment fragment = new ObjectFragment();
+				Bundle args = new Bundle();
+				args.putInt(POSITION, position);
+				fragment.setArguments(args);
+				return fragment;
 			}
 
 			@Override
 			public int getCount() {
 				Log.d(TAG, "getCount:");
-				return fragments.length;
+				return 6;
 			}
 
 			@Override
 			public CharSequence getPageTitle(int position) {
-				return fragments[position].getPageTitle();
+				return "Tab " + (position + 1);
 			}
 		};
 		final ActionBar actionBar = getActionBar();
 		actionBar.setDisplayHomeAsUpEnabled(true);
 		final ViewPager mViewPager = (ViewPager) findViewById(R.id.pager);
 		mViewPager.setAdapter(adapter);
-		for (int position=0;position<fragments.length;position++) {
-			fragments[position] = new ObjectFragment(position);
-			Bundle args = new Bundle();
-			args.putInt("position", position);
-			fragments[position].setArguments(args);
-		}
-	}
-
-	public static class ObjectFragment extends Fragment {
-		int position;
-
-		ObjectFragment(int position) {
-			this.position = position;
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			Bundle args = getArguments();
-			int position = args.getInt("position");
-			Log.d(TAG, "onCreateView: position=" + position);
-			View rootView = inflater.inflate(R.layout.fragment, container, false);
-			((TextView)rootView.findViewById(android.R.id.text1)).setText(Integer.toString(position * 2 + 1));
-			return rootView;
-		}
-
-		public CharSequence getPageTitle() {
-			Log.d(TAG, "getPageTitle: position=" + position);
-			return "Tab " + (position + 1);
-		}
 	}
 }
