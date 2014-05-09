0de5c983838a2c243a98661c14ab4104c6f2e5e2
M. Dietrich <mdt@emdete.de>
2014-05-09 17:06:17 +0200

    improve sample

--- tabulators/src/main/java/org/pyneo/android/gui/Sample.java
+++ tabulators/src/main/java/org/pyneo/android/gui/Sample.java
@@ -20,58 +20,62 @@ import android.widget.TextView;
 public class Sample extends FragmentActivity {
 	static final String TAG = Sample.class.getName();
 	static boolean DEBUG = true;
+	ObjectFragment[] fragments = new ObjectFragment[3];
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.d(TAG, "onCreate:");
 		setContentView(R.layout.main);
-		final FragmentStatePagerAdapter mDemoCollectionPagerAdapter = new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+		final FragmentStatePagerAdapter adapter = new FragmentStatePagerAdapter(getSupportFragmentManager()) {
 			@Override
-			public Fragment getItem(int i) {
-				Fragment fragment = new DemoObjectFragment();
-				Bundle args = new Bundle();
-				args.putInt("no", i + 1);
-				fragment.setArguments(args);
-				return fragment;
+			public Fragment getItem(int position) {
+				Log.d(TAG, "getItem: position=" + position);
+				return fragments[position];
 			}
 
 			@Override
 			public int getCount() {
-				return 10;
+				Log.d(TAG, "getCount:");
+				return fragments.length;
 			}
 
 			@Override
 			public CharSequence getPageTitle(int position) {
-				return "Tab " + (position + 1);
+				return fragments[position].getPageTitle();
 			}
 		};
 		final ActionBar actionBar = getActionBar();
 		actionBar.setDisplayHomeAsUpEnabled(true);
 		final ViewPager mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mDemoCollectionPagerAdapter);
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case android.R.id.home:
-				Intent upIntent = new Intent(this, Sample.class);
-				if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
-					TaskStackBuilder.from(this).addNextIntent(upIntent).startActivities();
-					finish();
-				} else {
-					NavUtils.navigateUpTo(this, upIntent);
-				}
-				return true;
+		mViewPager.setAdapter(adapter);
+		for (int position=0;position<fragments.length;position++) {
+			fragments[position] = new ObjectFragment(position);
+			Bundle args = new Bundle();
+			args.putInt("position", position);
+			fragments[position].setArguments(args);
 		}
-		return super.onOptionsItemSelected(item);
 	}
 
-	public static class DemoObjectFragment extends Fragment {
+	public static class ObjectFragment extends Fragment {
+		int position;
+
+		ObjectFragment(int position) {
+			this.position = position;
+		}
+
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment, container, false);
 			Bundle args = getArguments();
-			((TextView)rootView.findViewById(android.R.id.text1)).setText(Integer.toString(args.getInt("no") * 2 + 1));
+			int position = args.getInt("position");
+			Log.d(TAG, "onCreateView: position=" + position);
+			View rootView = inflater.inflate(R.layout.fragment, container, false);
+			((TextView)rootView.findViewById(android.R.id.text1)).setText(Integer.toString(position * 2 + 1));
 			return rootView;
+		}
+
+		public CharSequence getPageTitle() {
+			Log.d(TAG, "getPageTitle: position=" + position);
+			return "Tab " + (position + 1);
 		}
 	}
 }
