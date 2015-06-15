cb1152a585238febe2948da03c9c43029d5a0360
M. Dietrich <mdt@emdete.de>
2015-06-16 00:14:06 +0200

    add more fragments

---
+++ fragments/src/main/java/org/pyneo/android/gui/Map.java
@@ -0,0 +1,43 @@
+package org.pyneo.android.gui;
+
+import android.util.Log;
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class Map extends Base {
+	static final private String TAG = Sample.TAG;
+	static final private boolean DEBUG = Sample.DEBUG;
+	private boolean visible = true;
+
+	public void inform(int event, Bundle extra) {
+		if (DEBUG) { Log.d(TAG, "Map.inform event=" + event); }
+	}
+
+	@Override public void onAttach(Activity activity) {
+		if (DEBUG) { Log.d(TAG, "Map.onAttach"); }
+		super.onAttach(activity);
+	}
+
+	@Override public void onCreate(Bundle bundle) {
+		if (DEBUG) { Log.d(TAG, "Map.onCreate"); }
+		super.onCreate(bundle);
+	}
+
+	@Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		if (DEBUG) { Log.d(TAG, "Map.onCreateView"); }
+		return inflater.inflate(R.layout.map, container, false);
+	}
+
+	@Override public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		if (DEBUG) { Log.d(TAG, "Map.onActivityCreated"); }
+	}
+}
