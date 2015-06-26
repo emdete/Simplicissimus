fb7e740a6313364f9257a06d527301676aa4dde7
M. Dietrich <mdt@emdete.de>
2015-06-27 00:43:07 +0200

    use multiple map databases

--- mapsforge/02.fragment_getting_started/src/main/java/org/pyneo/android/gui/Map.java
+++ mapsforge/02.fragment_getting_started/src/main/java/org/pyneo/android/gui/Map.java
@@ -36,6 +36,7 @@ import org.mapsforge.map.layer.TileLayer;
 import org.mapsforge.map.model.common.PreferencesFacade;
 import org.mapsforge.map.model.MapViewPosition;
 import org.mapsforge.map.reader.MapDataStore;
+import org.mapsforge.map.reader.MultiMapDataStore;
 import org.mapsforge.map.reader.MapFile;
 import org.mapsforge.map.rendertheme.ExternalRenderTheme;
 import org.mapsforge.map.rendertheme.InternalRenderTheme;
@@ -46,7 +47,8 @@ public class Map extends Base {
 	static final private String TAG = Sample.TAG;
 	static final private boolean DEBUG = Sample.DEBUG;
 	// get one from http://download.mapsforge.org/maps/ and adapt path to your needs:
-	private static final String MAPFILE = "/storage/sdcard1/mapsforge/germany.map";
+	private static final String MAPFILE1 = "/storage/sdcard1/mapsforge/germany.map";
+	private static final String MAPFILE2 = "/storage/sdcard1/mapsforge/netherlands.map";
 	// leave out when not wanted:
 	private static final String THEMEFILE = "/storage/sdcard1/mapsforge/Tiramisu_3_0_beta1.xml";
 
@@ -82,8 +84,11 @@ public class Map extends Base {
 		// warp to 'unter den linden'
 		mapView.getModel().mapViewPosition.setCenter(new LatLong(52.517037, 13.38886));
 		mapView.getModel().mapViewPosition.setZoomLevel((byte)12);
-		tileLayer = new TileRendererLayer(tileCache, new MapFile(new File(MAPFILE)), mapView.getModel().mapViewPosition,
+		MultiMapDataStore multiMapDataStore = new MultiMapDataStore(MultiMapDataStore.DataPolicy.DEDUPLICATE);
+		tileLayer = new TileRendererLayer(tileCache, multiMapDataStore, mapView.getModel().mapViewPosition,
 			false, true, AndroidGraphicFactory.INSTANCE);
+		multiMapDataStore.addMapDataStore(new MapFile(new File(MAPFILE1)), true, true);
+		multiMapDataStore.addMapDataStore(new MapFile(new File(MAPFILE2)), false, false);
 		try {
 			tileLayer.setXmlRenderTheme(new ExternalRenderTheme(new File(THEMEFILE)));
 		}
