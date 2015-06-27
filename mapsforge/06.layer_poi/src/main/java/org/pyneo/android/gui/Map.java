e5543b424626a2075a13b6e224bb835e27f9b20d
M. Dietrich <mdt@emdete.de>
2015-06-27 02:16:30 +0200

    add poi sample

---
+++ mapsforge/06.layer_poi/src/main/java/org/pyneo/android/gui/Map.java
@@ -0,0 +1,97 @@
+package org.pyneo.android.gui;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.mapsforge.core.model.LatLong;
+import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
+import org.mapsforge.map.android.util.AndroidUtil;
+import org.mapsforge.map.android.view.MapView;
+import org.mapsforge.map.layer.cache.TileCache;
+import org.mapsforge.map.layer.renderer.TileRendererLayer;
+import org.mapsforge.map.reader.MapFile;
+import org.mapsforge.map.reader.MultiMapDataStore;
+import org.mapsforge.map.rendertheme.ExternalRenderTheme;
+import org.mapsforge.map.rendertheme.InternalRenderTheme;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Map extends Base {
+	static final private String TAG = Sample.TAG;
+	static final private boolean DEBUG = Sample.DEBUG;
+	// get one from http://download.mapsforge.org/maps/ and adapt path to your needs:
+	private static final String MAPFILE1 = "/storage/sdcard1/mapsforge/germany.map";
+	private static final String MAPFILE2 = "/storage/sdcard1/mapsforge/netherlands.map";
+	// leave out when not wanted:
+	private static final String THEMEFILE = "/storage/sdcard1/mapsforge/Tiramisu_3_0_beta1.xml";
+
+	MapView mapView;
+	TileRendererLayer tileLayer;
+	TileCache tileCache;
+
+	@Override public void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+		if (DEBUG) { Log.d(TAG, "Map.onCreate"); }
+		AndroidGraphicFactory.createInstance(getActivity().getApplication());
+		mapView = new MapView(getActivity());
+		//
+		mapView.setClickable(true);
+		mapView.getMapScaleBar().setVisible(true);
+		mapView.setBuiltInZoomControls(true);
+		mapView.getMapZoomControls().setZoomLevelMin((byte)2);
+		mapView.getMapZoomControls().setZoomLevelMax((byte)18);
+		mapView.getMapZoomControls().setShowMapZoomControls(true);
+		mapView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+		tileCache = AndroidUtil.createTileCache(getActivity(), "mapcache", mapView.getModel().displayModel.getTileSize(),
+			1f, mapView.getModel().frameBufferModel.getOverdrawFactor());
+	}
+
+	@Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		if (DEBUG) { Log.d(TAG, "Map.onCreateView"); }
+		return mapView;
+	}
+
+	@Override public void onStart() {
+		super.onStart();
+		if (DEBUG) { Log.d(TAG, "Map.onStart"); }
+		// warp to 'unter den linden'
+		mapView.getModel().mapViewPosition.setCenter(new LatLong(52.517037, 13.38886));
+		mapView.getModel().mapViewPosition.setZoomLevel((byte)12);
+		MultiMapDataStore multiMapDataStore = new MultiMapDataStore(MultiMapDataStore.DataPolicy.DEDUPLICATE);
+		tileLayer = new TileRendererLayer(tileCache, multiMapDataStore, mapView.getModel().mapViewPosition,
+			false, true, AndroidGraphicFactory.INSTANCE);
+		multiMapDataStore.addMapDataStore(new MapFile(new File(MAPFILE1)), true, true);
+		multiMapDataStore.addMapDataStore(new MapFile(new File(MAPFILE2)), false, false);
+		try {
+			tileLayer.setXmlRenderTheme(new ExternalRenderTheme(new File(THEMEFILE)));
+		}
+		catch (FileNotFoundException ignore) {
+			tileLayer.setXmlRenderTheme(InternalRenderTheme.OSMARENDER);
+		}
+		mapView.getLayerManager().getLayers().add(tileLayer);
+	}
+
+	@Override public void onStop() {
+		super.onStop();
+		if (DEBUG) Log.d(TAG, "onStop");
+		mapView.getLayerManager().getLayers().remove(tileLayer);
+		tileLayer.onDestroy();
+	}
+
+	@Override public void onDestroy() {
+		super.onDestroy();
+		if (DEBUG) { Log.d(TAG, "Map.onDestroy"); }
+		tileCache.destroy();
+		mapView.getModel().mapViewPosition.destroy();
+		mapView.destroy();
+		AndroidGraphicFactory.clearResourceMemoryCache();
+	}
+
+	public void inform(int event, Bundle extra) {
+		if (DEBUG) { Log.d(TAG, "Map.inform event=" + event); }
+	}
+}
